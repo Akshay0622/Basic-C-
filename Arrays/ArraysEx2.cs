3cf35bee4d5460f2302cf06ea389cce3ebe8129c
Akshay0622 <akshaytanpure226@gmail.com>
2024-03-30 00:01:47 +0530

    Array

---
+++ Arrays/ArraysEx2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class ArraysEx2
+    { public static void Main(string[] args)
+        {
+
+            int[] allvalue = { 40, 40, 40, 40, 40, 40 };
+
+
+            int maxvalue = int.MinValue;
+            int secondMaxvalue = int.MinValue;
+
+            for (int i = 0; i < allvalue.Length; i++)
+            {
+                if (allvalue[i] > maxvalue)
+                {
+                    secondMaxvalue = maxvalue;
+                    maxvalue = allvalue[i];
+                }
+                else if (allvalue[i] > secondMaxvalue && allvalue[i] != maxvalue)
+                {
+
+                    secondMaxvalue = allvalue[i];
+                }
+            }
+
+                Console.WriteLine("2nd max value:" + secondMaxvalue);
+         }
+    }
+}
