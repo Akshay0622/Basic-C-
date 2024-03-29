64241950e41867033e60a841e23d8055ef33868e
Akshay0622 <akshaytanpure226@gmail.com>
2024-03-30 00:04:17 +0530

    array example

---
+++ Arrays/ArraysEx4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+   
+    internal class ArraysEx4
+    {
+        public static void Main(string[] args)
+
+        {
+            String[] months = { "jan", "feb", "march", "april", "may", "june", "july" };
+
+            Console.WriteLine(months[3]);
+
+        }
+        }
+    }
