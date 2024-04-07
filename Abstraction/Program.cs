a44ff28809fd0f232b0a527e2ba6c6431f94ee6e
Akshay0622 <akshaytanpure226@gmail.com>
2024-04-08 00:01:34 +0530

    interface and abstraction done

---
+++ Abstraction/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Vehicle vehicle = new Car();
+            vehicle.run();
+        }
+    }
+}
---
+++ InterfaceEx/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace InterfaceEx
+{
+    public class Person : IPerson
+    {
+        // Properties implementation
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // Method implementation
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+}
