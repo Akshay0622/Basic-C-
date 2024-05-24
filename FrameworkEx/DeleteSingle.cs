24f520daf90de1e771f7d63baee12f86b271595c
Akshay0622 <akshaytanpure226@gmail.com>
2024-05-25 00:04:22 +0530

    framewr

---
+++ FrameworkEx/DeleteSingle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkEx
+{
+    internal class DeleteSingle
+    {
+
+
+        public static void Main(string[] args)
+        {
+
+
+            ProductContext productContext = new ProductContext();
+
+            Console.WriteLine("Enter name of product :- ");
+            String name=Console.ReadLine();
+
+            try
+            {
+                Product product = productContext.Products.Single(product => product.name == name);
+
+                Console.WriteLine(product);
+
+                productContext.Products.Remove(product);
+
+                productContext.SaveChanges();
+
+                Console.WriteLine("Record deleted");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("No matching record found");
+            }
+
+        }
+    }
+}
---
+++ FrameworkEx/ReadingBasedOnCondition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkEx
+{
+    internal class ReadingBasedOnCondition
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            ProductContext productContext = new ProductContext();
+
+
+            try
+            {
+                List<Product> list = productContext.Products.Where(product => product.price>5).ToList();
+
+                Console.WriteLine(list);
+
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("No matching record found");
+            }
+
+        }
+
+    }
+}
