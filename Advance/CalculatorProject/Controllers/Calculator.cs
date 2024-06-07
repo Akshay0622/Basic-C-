3ebbdf617d3200f70c16393303b5463e5a7fc82c
Akshay0622 <akshaytanpure226@gmail.com>
2024-06-08 00:00:42 +0530

    new file

---
+++ Advance/CalculatorProject/Controllers/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorProject.Controllers
+{
+    public class Calculator : Controller
+    {
+        public IActionResult Add(int no1,int no2)
+
+        {
+            int answer = no1 + no2;
+            ViewData["no1"] = no1;
+            ViewData["no2"] = no2;
+            ViewData["answer"] = answer;
+
+            return View();
+        }
+    }
+}
